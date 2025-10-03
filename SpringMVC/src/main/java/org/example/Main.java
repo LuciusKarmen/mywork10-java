d4c72cbf658595a7ebbd41032d1b657214bb5375
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-10-03 23:40:26 +0800

    10.3

--- SpringMVC/src/main/java/org/example/Main.java
+++ SpringMVC/src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ package org.example;
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-        // spring事务AIDC
-        
 
     }
 }
