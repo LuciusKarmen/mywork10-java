ba0de47ca3733174a2a68c2dc57bcde634435a36
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-10-05 00:42:07 +0800

    10.5

---
+++ SpringBoot/src/main/java/com/xingchengnet/controller/Test.java
@@ -0,0 +1,19 @@
+package com.xingchengnet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Test {
+
+    @GetMapping("/{id}")
+    public String getID(@PathVariable Integer id){
+        System.out.println("id==>"+id);
+        return "Hello,Karmen!";
+
+    }
+
+}
